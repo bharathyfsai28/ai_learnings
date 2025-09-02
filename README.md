b8358226f9c1d75973897b5d15f86d4898e64d7e
Bharathy Poovalingam <bharathy.poovalingam@futuresecure.ai>
2025-09-02 11:01:24 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## Bootstrap the Project
+
+    npm init -y
+
+## Install Dependencies
+
+    npm i langchain @langchain/openai
+    npm i dotenv
+    npm i nodemon --save-dev
+
+
+## Modify Package.json
+
+    modify package.json file to add below line ( to support import statements )
+        - "type" : "module"
+
+
+## Create the Key
+
+    https://platform.openai.com/api-keys
---
+++ langchain-spike/llm.js
@@ -0,0 +1,18 @@
+import { ChatOpenAI } from "@langchain/openai";
+import * as dotenv from 'dotenv'
+
+
+// GET THE KEY FROM ENV VARIABLE
+dotenv.config();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+console.log(OPENAI_API_KEY);
+// Define the model
+
+const model = new ChatOpenAI({
+
+    openAIApiKey : OPENAI_API_KEY
+    
+});
+
+const response = await model.invoke('What is LLM?');
+console.log(response);
