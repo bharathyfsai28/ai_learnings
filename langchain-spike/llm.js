a074a1bc63bdc72bc6c9624443a57c468903cda6
Bharathy Poovalingam <bharathy.poovalingam@futuresecure.ai>
2025-09-02 11:12:36 +1000

    docs updated

--- langchain-spike/llm.js
+++ langchain-spike/llm.js
@@ -2,10 +2,13 @@ import { ChatOpenAI } from "@langchain/openai";
 import * as dotenv from 'dotenv'
 
 
-// GET THE KEY FROM ENV VARIABLE
+// GET THE KEY FROM ENV file
+
 dotenv.config();
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 console.log(OPENAI_API_KEY);
+
+
 // Define the model
 
 const model = new ChatOpenAI({
